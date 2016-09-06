00ebc8fa425a209311b55abf5915d58f61292c59
Kevin Pfefferle <kevin@pfefferle.co>
2016-09-06 19:25:54 -0400

    List items

---
+++ app/controllers/application.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  items: [
+    {
+      title: 'Item 1'
+    },
+    {
+      title: 'Item 2'
+    },
+    {
+      title: 'Item 3'
+    },
+    {
+      title: 'Item 4'
+    },
+    {
+      title: 'Item 5'
+    }
+  ]
+});
