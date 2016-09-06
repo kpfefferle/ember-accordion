16a794338166a9a3ba2908df27a22ff84d9665bd
Kevin Pfefferle <kevin@pfefferle.co>
2016-09-06 19:35:39 -0400

    Item content

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,19 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   items: [
     {
-      title: 'Item 1'
+      title: 'Item 1',
+      content: 'Content 1'
     },
     {
-      title: 'Item 2'
+      title: 'Item 2',
+      content: 'Content 2'
     },
     {
-      title: 'Item 3'
+      title: 'Item 3',
+      content: 'Content 3'
     },
     {
-      title: 'Item 4'
+      title: 'Item 4',
+      content: 'Content 4'
     },
     {
-      title: 'Item 5'
+      title: 'Item 5',
+      content: 'Content 5'
     }
   ]
 });
