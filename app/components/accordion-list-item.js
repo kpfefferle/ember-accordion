f03d04518b083c71ac564f3d48b9c5754b2a7772
Kevin Pfefferle <kevin@pfefferle.co>
2016-09-06 19:32:44 -0400

    Open/close

--- app/components/accordion-list-item.js
+++ app/components/accordion-list-item.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  item: {}
+  item: {},
+
+  isOpen: false,
+
+  click() {
+    this.toggleProperty('isOpen');
+  }
 });
